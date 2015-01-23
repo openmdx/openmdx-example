62556c4d95e4fca350ac08d52c632161febdeb4a
Werner Froidevaux <wfro@users.sourceforge.net>
2015-01-23 18:48:34 +0100

    OPENMDX_EXAMPLE_2_14_0

--- workshop/etc/data/workshop/startdb.sh
+++ workshop/etc/data/workshop/startdb.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -cp ../../../../opt/hsqldb/jre-1.6/hsqldb-2/lib/hsqldb.jar org.hsqldb.server.Server --port 9002 --database.0 file:workshop --dbname.0 WORKSHOP
+java -cp ../../../../opt/hsqldb/jre-1.7/hsqldb-2/lib/hsqldb.jar org.hsqldb.server.Server --port 9002 --database.0 file:workshop --dbname.0 WORKSHOP
--- workshop/etc/data/workshop/startdbmanager.sh
+++ workshop/etc/data/workshop/startdbmanager.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -cp ../../../../opt/hsqldb/jre-1.6/hsqldb-2/lib/hsqldb.jar org.hsqldb.util.DatabaseManagerSwing --url jdbc:hsqldb:hsql://127.0.0.1:9002/WORKSHOP --user sa --password "manager99"
+java -cp ../../../../opt/hsqldb/jre-1.7/hsqldb-2/lib/hsqldb.jar org.hsqldb.util.DatabaseManagerSwing --url jdbc:hsqldb:hsql://127.0.0.1:9002/WORKSHOP --user sa --password "manager99"
