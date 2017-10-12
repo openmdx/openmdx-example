893b9ad68b65e8e40d3108a782df860b7a29d528
Werner Froidevaux <wfro@users.sourceforge.net>
2017-10-12 11:33:24 +0200

    OPENMDX_2_15_7

--- workshop/etc/data/workshop/startdb.bat
+++ workshop/etc/data/workshop/startdb.bat
@@ -1,1 +1,1 @@
-java -cp ..\..\..\..\opt\hsqldb\jre-1.7\hsqldb-2\lib\hsqldb.jar org.hsqldb.server.Server --port 9002 --database.0 file:workshop --dbname.0 WORKSHOP
+java -cp ..\..\..\..\opt\hsqldb\jre-1.8\hsqldb-2\lib\hsqldb.jar org.hsqldb.server.Server --port 9002 --database.0 file:workshop --dbname.0 WORKSHOP
